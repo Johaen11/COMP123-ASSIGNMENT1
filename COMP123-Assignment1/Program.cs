a1754a8491d7e58a69dceadb3266a962fdb5b96e
johaen11 <johngnanakumar@gmail.com>
2016-05-28 17:15:57 -0400

    Added Instance variables and public property Name to the Hero Class.

--- COMP123-Assignment1/Program.cs
+++ COMP123-Assignment1/Program.cs
@@ -3,7 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/**
+ * Author's Name: Johaen Gnanakumar
+ * Author's Student#:300880309
+ * Date Last Modified:05/28/2016
+ * Program Description:
+ * Revision History: Version 0.01
+ */ 
 namespace COMP123_Assignment1
 {
     class Program
